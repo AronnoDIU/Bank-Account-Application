2816b49017fc69af98ff479ddb000807aa81dbe6
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-11-19 01:51:19 +0600

    Bank Account Application

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -3,6 +3,7 @@ import java.util.List;
 
 public class BankAccount {
     public static void main(String[] args) {
+        // Create an empty list of accounts
         List<Account> accounts = new ArrayList<>();
 
         // Read a CSV File, then create new accounts based on that data
@@ -38,31 +39,3 @@ public class BankAccount {
         }
     }
 }
-
-/* Expected output:
-
-Application should do the following:
-
-    Read a .csv file of names, social security numbers, account type, and initial deposit
-    Use a proper data structure to hold all these accounts
-    Both savings and checking accounts share the following properties
-        deposit()
-        withdraw()
-        transfer()
-        showInfo()
-    Each account has a unique 10-digit account number that is generated with the following process:
-        1 or 2 depending on Savings or Checking, last two digits of SSN, unique 5-digit number,
-        and random 3-digit number,
-        3 or 4 depending on Savings or Checking, last two digits of SSN, unique 5-digit number,
-        11-Digit Account Number (generated with the following process: 1 or 2 depending on
-        Savings or Checking, last two digits of SSN, unique 5-digit number, and random 3-digit number)
-    Savings Account holders are given a Safe deposit box, identified by a 3-digit number and
-    assigned a 6-digit account number that is accessed with a 4-digit code
-    Checking Account holders are assigned a Debit Card with a 12-digit number and 4-digit PIN
-    Both accounts will use an interface that determines the base interest rate.
-        Saving an account will use .25 points less than the base rate
-        Checking accounts will use 15% of the base rate
-    The showInfo() method should reveal relevant account information as well as
-    other information specific to the Checking account or Savings account
-
-*/
